f8da18c29336f4a0908273ff38d737d26e34f066
meda123 <mrt62@cornell.edu>
2017-05-17 10:25:50 -0700

    First attemp, fail fast if words are not the same length.

--- 1_2_checkPermutation.py
+++ 1_2_checkPermutation.py
@@ -3,4 +3,27 @@ Task: Given two strings, write a method to decide if one is a permutation of
 the other.
 
 
+Hint: Two strings that are permutations should have the same characters but in 
+different orders. Can you make the orders the same? 
 """
+word1 = "hello"
+word2 = "eholl"
+
+
+same = {}
+
+def same_permutation(word1, word2):
+
+    #Fail fast check, same length? 
+    if len(word1) != len(word2):
+        return False
+
+    #Order word2 to look like word1 
+    for i in range(len(word2)): 
+      
+        word_2_change = word2[i+1:len(word2)] + word2[i]
+        print word_2_change
+
+same_permutation(word1, word2) 
+
+
