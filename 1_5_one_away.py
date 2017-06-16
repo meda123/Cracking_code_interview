6e4d7ef0b8bc0ba4f22568582a05a418756b6c2f
meda123 <mrt62@cornell.edu>
2017-06-15 23:49:21 -0700

    initial commit.

---
+++ 1_5_one_away.py
@@ -0,0 +1,22 @@
+
+"""
+One away: There are three types of edits that can be performed on strings:
+insert a character, remove a character, or replace a character. Give two 
+strings, write a function to check if they are one edit( or zero edits) away.
+
+>>> one_away("pale", "ple")
+True 
+
+>>> one_away("pales", "pale")
+True 
+
+>>> one_away("pale", "bale")
+True 
+
+>>> one_away("pale", "bake")
+False 
+
+"""
+
+def one_away():
+    pass 
