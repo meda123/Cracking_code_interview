d041f3c3344146837b6f408342ad722aa23894dd
meda123 <mrt62@cornell.edu>
2017-06-27 22:10:44 -0700

    Added docstrings.

--- 1_4_palindrome_permutation.py
+++ 1_4_palindrome_permutation.py
@@ -5,26 +5,45 @@ A palindrome is a word or phrase that is the same forwards and backwards. A
 permutation is a rearrangement of letters. The palindrome does not need to be 
 limited to just dictionary words.
 
->>> pal_permutation("Tact Coa")
-True 
-
-(permutations: "taco cat", "atco cta", etc)
-
 """
 
 from collections import Counter 
 
-example = "Tact Coa"
-exa2 = "taco cat"
 
-def pal_permutation(words, permutation):
+def pal_permutation(words):
+    """ 
+    >>> pal_permutation("Mama paPa")
+    True
+
+    >>> pal_permutation("Medalis Trelles")
+    False
+
+    >>> pal_permutation("Taco Cat")
+    True
+
+    """
 
     words = words.replace(' ', '').lower()
 
-    return sum(freq%2 for freq in Counter(words).values()) < 2 
+    return sum(freq%2 for freq in Counter(words).values()) < 2   
 
 
-test = pal_permutation(example,exa2)
-print test 
+"""
+Approach:
 
 
+
+"""
+
+
+
+##################### DocTests ###########################################
+
+if __name__ == "__main__":
+    import doctest
+
+    print
+    result = doctest.testmod()
+    if not result.failed:
+        print "ALL TESTS PASSED. GOOD WORK!"
+    print
