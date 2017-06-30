af97a269b49a872d9ab7f4c40d0db43c6ad52562
meda123 <mrt62@cornell.edu>
2017-06-29 23:37:44 -0700

    Added doctests.

--- Int_Challenges/1_edtech.py
+++ Int_Challenges/1_edtech.py
@@ -1,15 +1,29 @@
 
-# Coding challenge 06/26/16 
+""" 
+Coding challenge 06/26/16 
 
+Given a number, return how many holes are associated with each indivial
+number and return an integer with the total count 
+
+"""
 holes = {"1":0, "2":0, "3":0, "5":0, 7:0, 
         "0":1, "4":1, "6":1, "9":1,
         "8":2}
 
-def countHoles(num):
+def count_holes(num):
+    """
+    Function counts how many holes are hit in a series of numbers
+    
+    >>> count_holes(0)
+    1
 
-"""
-Function counts how many holes are hit in a series of numbers
-"""
+    >>> count_holes(630)
+    2
+
+    >>> count_holes(6308)
+    4
+
+    """
 
     count = 0 
 
@@ -22,5 +36,15 @@ Function counts how many holes are hit in a series of numbers
     return count
 
 
-test = countHoles(630)
-print test 
+
+################### DocTests ###########################################
+
+if __name__ == "__main__":
+    import doctest
+
+    print
+    result = doctest.testmod()
+    if not result.failed:
+        print "ALL TESTS PASSED. GOOD WORK!"
+    print
+
